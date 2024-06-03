318d2ff307ff95e54ca710119d624b25ee5bd9b0
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-06-03 15:47:04 -0700

    fix multiversion

--- root.gradle.kts
+++ root.gradle.kts
@@ -8,6 +8,6 @@ preprocess {
     val fabric12004 = createNode("1.20.4-fabric", 12004, "yarn")
     val fabric12001 = createNode("1.20.1-fabric", 12001, "yarn")
 
-    fabric12004.link(fabric12006)
-    fabric12001.link(fabric12004)
+    fabric12006.link(fabric12004)
+    fabric12004.link(fabric12001)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,9 +7,9 @@ pluginManagement {
 }
 
 listOf(
-	"1.20.6-fabric",
+	"1.20.1-fabric",
 	"1.20.4-fabric",
-	"1.20.1-fabric"
+	"1.20.6-fabric"
 ).forEach { version ->
 	include(":$version")
 	project(":$version").apply {
