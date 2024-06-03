a6e5adaffe2c71b3107566d561707975f595e28d
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-06-03 16:01:00 -0700

    fix mainproject

--- src/main/java/dev/microcontrollers/scoreboardtweaks/mixin/InGameHudMixin.java
+++ src/main/java/dev/microcontrollers/scoreboardtweaks/mixin/InGameHudMixin.java
@@ -3,7 +3,6 @@ package dev.microcontrollers.scoreboardtweaks.mixin;
 import dev.microcontrollers.scoreboardtweaks.config.ScoreboardTweaksConfig;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.scoreboard.ScoreboardObjective;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,8 +10,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
-    @Inject(method = "renderScoreboardSidebar", at = @At("HEAD"), cancellable = true)
-    private void cancelScoreboardRendering(DrawContext context, ScoreboardObjective objective, CallbackInfo ci) {
+    @Inject(method = "renderScoreboardSidebar(Lnet/minecraft/client/gui/DrawContext;F)V", at = @At("HEAD"), cancellable = true)
+    private void cancelScoreboardRendering(DrawContext context, float tickDelta, CallbackInfo ci) {
         if (ScoreboardTweaksConfig.CONFIG.instance().removeScoreboard) ci.cancel();
     }
 }
