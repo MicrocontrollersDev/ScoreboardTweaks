c22132055f0878ad0565fd7eff158eca6686a4df
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-06-12 18:34:15 -0700

    push changes, remove 1.20.1

--- src/main/java/dev/microcontrollers/scoreboardtweaks/config/ScoreboardTweaksConfig.java
+++ src/main/java/dev/microcontrollers/scoreboardtweaks/config/ScoreboardTweaksConfig.java
@@ -5,9 +5,12 @@ import dev.isxander.yacl3.api.controller.TickBoxControllerBuilder;
 import dev.isxander.yacl3.config.v2.api.ConfigClassHandler;
 import dev.isxander.yacl3.config.v2.api.SerialEntry;
 import dev.isxander.yacl3.config.v2.api.serializer.GsonConfigSerializerBuilder;
+import dev.isxander.yacl3.gui.controllers.ColorController;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.Text;
+
+import java.awt.Color;
 
 public class ScoreboardTweaksConfig {
     public static final ConfigClassHandler<ScoreboardTweaksConfig> CONFIG = ConfigClassHandler.createBuilder(ScoreboardTweaksConfig.class)
@@ -17,21 +20,91 @@ public class ScoreboardTweaksConfig {
             .build();
 
     @SerialEntry public boolean removeScoreboard = false;
+    @SerialEntry public boolean removeScoreboardInDebugHud = false;
+//    @SerialEntry public float scale = 1F;
+    @SerialEntry public boolean removeScore = false;
+//    @SerialEntry public boolean removeOnlySequentialScore = true;
+    @SerialEntry public Color scoreColor = new Color(16733525);
+    @SerialEntry public Color headerColor = new Color(0, 0, 0, 102);
+    @SerialEntry public Color bodyColor = new Color(0, 0, 0, 76);
+    @SerialEntry public boolean headerShadow = false;
+    @SerialEntry public boolean bodyShadow = false;
+    @SerialEntry public boolean numberShadow = false;
 
+    @SuppressWarnings("deprecation")
     public static Screen configScreen(Screen parent) {
         return YetAnotherConfigLib.create(CONFIG, ((defaults, config, builder) -> builder
                 .title(Text.literal("Scoreboard Tweaks"))
                 .category(ConfigCategory.createBuilder()
                         .name(Text.literal("Scoreboard Tweaks"))
-
-                        // Visual
-
                         .group(OptionGroup.createBuilder()
                                 .name(Text.literal("General"))
                                 .option(Option.createBuilder(boolean.class)
                                         .name(Text.literal("Remove Scoreboard"))
                                         .description(OptionDescription.of(Text.of("Remove the scoreboard entirely.")))
                                         .binding(defaults.removeScoreboard, () -> config.removeScoreboard, newVal -> config.removeScoreboard = newVal)
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+                                .option(Option.createBuilder(boolean.class)
+                                        .name(Text.literal("Remove Scoreboard in Debug HUD"))
+                                        .description(OptionDescription.of(Text.of("Remove the scoreboard when the debug / F3 menu is open.")))
+                                        .binding(defaults.removeScoreboardInDebugHud, () -> config.removeScoreboardInDebugHud, newVal -> config.removeScoreboardInDebugHud = newVal)
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+//                                .option(Option.createBuilder(float.class)
+//                                        .name(Text.literal("Scoreboard Scale"))
+//                                        .description(OptionDescription.of(Text.of("Scales the scoreboard by the specified amount.")))
+//                                        .binding(1F, () -> config.scale, newVal -> config.scale = newVal)
+//                                        .controller(opt -> FloatSliderControllerBuilder.create(opt)
+//                                                .range(0.1F, 3F)
+//                                                .step(0.1F))
+//                                        .build())
+                                .option(Option.createBuilder(boolean.class)
+                                        .name(Text.literal("Remove Score"))
+                                        .description(OptionDescription.of(Text.of("Remove the score entirely.")))
+                                        .binding(defaults.removeScore, () -> config.removeScore, newVal -> config.removeScore = newVal)
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+//                                .option(Option.createBuilder(boolean.class)
+//                                        .name(Text.literal("Remove Only Sequential Score"))
+//                                        .description(OptionDescription.of(Text.of("Remove the score only when the numbers are sequential. This means the numbers won't show when on, for example, Hypxiel lobbies where they only exist for ordering the components.")))
+//                                        .binding(defaults.removeOnlySequentialScore, () -> config.removeOnlySequentialScore, newVal -> config.removeOnlySequentialScore = newVal)
+//                                        .controller(TickBoxControllerBuilder::create)
+//                                        .build())
+                                .option(Option.<Color>createBuilder()
+                                        .name(Text.literal("Number Color"))
+                                        .description(OptionDescription.of(Text.of("Number color.")))
+                                        .binding(defaults.scoreColor, () -> config.scoreColor, value -> config.scoreColor = value)
+                                        .customController(opt -> new ColorController(opt, true))
+                                        .build())
+                                .option(Option.<Color>createBuilder()
+                                        .name(Text.literal("Header Color"))
+                                        .description(OptionDescription.of(Text.of("Header color.")))
+                                        .binding(defaults.headerColor, () -> config.headerColor, value -> config.headerColor = value)
+                                        .customController(opt -> new ColorController(opt, true))
+                                        .build())
+                                .option(Option.<Color>createBuilder()
+                                        .name(Text.literal("Body Color"))
+                                        .description(OptionDescription.of(Text.of("Body color.")))
+                                        .binding(defaults.bodyColor, () -> config.bodyColor, value -> config.bodyColor = value)
+                                        .customController(opt -> new ColorController(opt, true))
+                                        .build())
+                                .option(Option.createBuilder(boolean.class)
+                                        .name(Text.literal("Add Header Text Shadow"))
+                                        .description(OptionDescription.of(Text.of("Add shadow to the header text.")))
+                                        .binding(defaults.headerShadow, () -> config.headerShadow, newVal -> config.headerShadow = newVal)
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+                                .option(Option.createBuilder(boolean.class)
+                                        .name(Text.literal("Add Body Text Shadow"))
+                                        .description(OptionDescription.of(Text.of("Add shadow to the body text.")))
+                                        .binding(defaults.bodyShadow, () -> config.bodyShadow, newVal -> config.bodyShadow = newVal)
+                                        .controller(TickBoxControllerBuilder::create)
+                                        .build())
+                                .option(Option.createBuilder(boolean.class)
+                                        .name(Text.literal("Add Number Text Shadow"))
+                                        .description(OptionDescription.of(Text.of("Add shadow to the number text.")))
+                                        .binding(defaults.numberShadow, () -> config.numberShadow, newVal -> config.numberShadow = newVal)
                                         .controller(TickBoxControllerBuilder::create)
                                         .build())
                                 .build())
--- src/main/java/dev/microcontrollers/scoreboardtweaks/mixin/InGameHudMixin.java
+++ src/main/java/dev/microcontrollers/scoreboardtweaks/mixin/InGameHudMixin.java
@@ -1,17 +1,76 @@
 package dev.microcontrollers.scoreboardtweaks.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import dev.microcontrollers.scoreboardtweaks.config.ScoreboardTweaksConfig;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.scoreboard.*;
+import net.minecraft.scoreboard.number.NumberFormat;
+import net.minecraft.scoreboard.number.StyledNumberFormat;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+// TODO: remember to do multiversion fixes
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
-    @Inject(method = "renderScoreboardSidebar(Lnet/minecraft/client/gui/DrawContext;F)V", at = @At("HEAD"), cancellable = true)
-    private void cancelScoreboardRendering(DrawContext context, float tickDelta, CallbackInfo ci) {
+    @Unique
+    MinecraftClient client = MinecraftClient.getInstance();
+
+    @Inject(method = "renderScoreboardSidebar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/scoreboard/ScoreboardObjective;)V", at = @At("HEAD"), cancellable = true)
+    private void cancelScoreboardRendering(DrawContext context, ScoreboardObjective objective, CallbackInfo ci) {
         if (ScoreboardTweaksConfig.CONFIG.instance().removeScoreboard) ci.cancel();
+        //#if MC >= 1.20.4
+        if (ScoreboardTweaksConfig.CONFIG.instance().removeScoreboardInDebugHud && client.getDebugHud().shouldShowDebugHud()) {
+        //#else
+        //$$ if (ScoreboardTweaksConfig.CONFIG.instance().removeScoreboardInDebugHud && MinecraftClient.getInstance().options.debugEnabled) {
+        //#endif
+            ci.cancel();
+        }
     }
+
+    @ModifyExpressionValue(method = "renderScoreboardSidebar(Lnet/minecraft/client/gui/DrawContext;Lnet/minecraft/scoreboard/ScoreboardObjective;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/scoreboard/ScoreboardObjective;getNumberFormatOr(Lnet/minecraft/scoreboard/number/NumberFormat;)Lnet/minecraft/scoreboard/number/NumberFormat;"))
+    private NumberFormat changeNumberColor(NumberFormat original) {
+        return new StyledNumberFormat(Style.EMPTY.withColor(ScoreboardTweaksConfig.CONFIG.instance().scoreColor.getRGB()));
+    }
+
+    // TODO: sequential score check
+    @WrapWithCondition(method = "method_55440", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 2))
+    private boolean removeScore(DrawContext instance, TextRenderer textRenderer, Text text, int x, int y, int color, boolean shadow) {
+        return !ScoreboardTweaksConfig.CONFIG.instance().removeScore;
+    }
+
+    @ModifyArg(method = "method_55440", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V", ordinal = 0), index = 4)
+    private int changeHeaderColor(int color) {
+        return ScoreboardTweaksConfig.CONFIG.instance().headerColor.getRGB();
+    }
+
+    @ModifyArg(method = "method_55440", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;fill(IIIII)V", ordinal = 1), index = 4)
+    private int changeBodyColor(int color) {
+        return ScoreboardTweaksConfig.CONFIG.instance().bodyColor.getRGB();
+    }
+
+    @ModifyArg(method = "method_55440", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 0), index = 5)
+    private boolean headerShadow(boolean shadow) {
+        return ScoreboardTweaksConfig.CONFIG.instance().headerShadow;
+    }
+
+    @ModifyArg(method = "method_55440", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 1), index = 5)
+    private boolean bodyShadow(boolean shadow) {
+        return ScoreboardTweaksConfig.CONFIG.instance().bodyShadow;
+    }
+
+    @ModifyArg(method = "method_55440", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawText(Lnet/minecraft/client/font/TextRenderer;Lnet/minecraft/text/Text;IIIZ)I", ordinal = 2), index = 5)
+    private boolean numberShadow(boolean shadow) {
+        return ScoreboardTweaksConfig.CONFIG.instance().numberShadow;
+    }
+
 }
